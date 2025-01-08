32b2da8689499fb086054e14d759718427f724dc
JaySon <tshent@qq.com>
2025-01-08 18:09:32 +0800

    *: Make tiflash compatible with the cse branch (#339)
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- dbms/src/Storages/KVStore/TiKVHelpers/DecodedLockCFValue.cpp
+++ dbms/src/Storages/KVStore/TiKVHelpers/DecodedLockCFValue.cpp
@@ -59,7 +59,11 @@ inline void decodeLockCfValue(DecodedLockCFValue & res)
             {
             case SHORT_VALUE_PREFIX:
             {
+#if SERVERLESS_PROXY != 0
+                size_t str_len = readVarUInt(data, len);
+#else
                 size_t str_len = readUInt8(data, len);
+#endif
                 if (len < str_len)
                     throw Exception("content len shorter than short value len", ErrorCodes::LOGICAL_ERROR);
                 // no need short value
