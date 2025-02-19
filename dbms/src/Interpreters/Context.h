3a2338046a692ea80f0d49e98a4f6fe2056c22bb
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2025-02-19 23:32:16 +0800

    *: Set ApplicationType and disagg param when GlobalContext created (#9886) (#9890)
    ref pingcap/tiflash#9857
    
    Signed-off-by: ti-chi-bot <ti-community-prow-bot@tidb.io>
    Signed-off-by: JaySon-Huang <tshent@qq.com>
    
    Co-authored-by: JaySon <tshent@qq.com>
    Co-authored-by: JaySon-Huang <tshent@qq.com>

--- dbms/src/Interpreters/Context.h
+++ dbms/src/Interpreters/Context.h
@@ -168,6 +168,7 @@ private:
 
     UInt64 session_close_cycle = 0;
     bool session_is_used = false;
+    bool is_config_loaded = false; /// Is configuration loaded from toml file.
 
     enum TestMode
     {
@@ -196,9 +197,21 @@ private:
     Context();
 
 public:
+    enum class ApplicationType
+    {
+        SERVER, /// The program is run as clickhouse-server daemon (default behavior)
+        CLIENT, /// clickhouse-client
+        LOCAL /// clickhouse-local
+    };
+
     /// Create initial Context with ContextShared and etc.
-    static std::unique_ptr<Context> createGlobal(std::shared_ptr<IRuntimeComponentsFactory> runtime_components_factory);
-    static std::unique_ptr<Context> createGlobal();
+    static std::unique_ptr<Context> createGlobal(
+        std::shared_ptr<IRuntimeComponentsFactory> runtime_components_factory,
+        ApplicationType app_type,
+        const std::optional<DisaggOptions> & disagg_opt);
+    static std::unique_ptr<Context> createGlobal(
+        ApplicationType app_type,
+        const std::optional<DisaggOptions> & disagg_opt = std::nullopt);
 
     ~Context();
 
@@ -506,16 +519,6 @@ public:
 
     void shutdown();
 
-    enum class ApplicationType
-    {
-        SERVER, /// The program is run as clickhouse-server daemon (default behavior)
-        CLIENT, /// clickhouse-client
-        LOCAL /// clickhouse-local
-    };
-
-    ApplicationType getApplicationType() const;
-    void setApplicationType(ApplicationType type);
-
     /// Sets default_profile and system_profile, must be called once during the initialization
     void setDefaultProfiles(const Poco::Util::AbstractConfiguration & config);
     String getDefaultProfileName() const;
@@ -561,8 +564,6 @@ public:
     const std::shared_ptr<DB::DM::SharedBlockSchemas> & getSharedBlockSchemas() const;
     void initializeSharedBlockSchemas(size_t shared_block_schemas_size);
 
-    void mockConfigLoaded() { is_config_loaded = true; }
-
     void initKeyspaceBlocklist(const std::unordered_set<KeyspaceID> & keyspace_ids);
     bool isKeyspaceInBlocklist(KeyspaceID keyspace_id);
     void initRegionBlocklist(const std::unordered_set<RegionID> & region_ids);
@@ -587,8 +588,6 @@ private:
     void scheduleCloseSession(const SessionKey & key, std::chrono::steady_clock::duration timeout);
 
     void checkIsConfigLoaded() const;
-
-    bool is_config_loaded = false; /// Is configuration loaded from toml file.
 };
 
 
