8f886c598adb480616c3b8d8e963739b29359954
Ray Yan <yming0221@gmail.com>
2026-01-07 09:58:20 +0800

    MemoryTracker: exclude rss_file for memory check limit & refresh jemalloc stats (#478)
    Signed-off-by: yongman <yming0221@gmail.com>

--- dbms/src/Common/BackgroundTask.cpp
+++ dbms/src/Common/BackgroundTask.cpp
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include <Common/BackgroundTask.h>
+#include <ProcessMetrics/ProcessMetrics.h>
 
 #include <fstream>
 
@@ -20,10 +21,8 @@ namespace DB
 {
 namespace
 {
-bool process_mem_usage(double & resident_set, Int64 & cur_proc_num_threads, UInt64 & cur_virt_size)
+bool process_num_threads(Int64 & cur_proc_num_threads)
 {
-    resident_set = 0.0;
-
     // 'file' stat seems to give the most reliable results
     std::ifstream stat_stream("/proc/self/stat", std::ios_base::in);
     // if "/proc/self/stat" is not supported
@@ -36,17 +35,15 @@ bool process_mem_usage(double & resident_set, Int64 & cur_proc_num_threads, UInt
     std::string utime, stime, cutime, cstime, priority, nice;
     std::string itrealvalue, starttime;
 
-    // the field we want
-    Int64 rss;
+    // the fields we want
+    UInt64 cur_virt_size = 0;
+    Int64 rss = 0;
 
     stat_stream >> pid >> comm >> state >> ppid >> pgrp >> session >> tty_nr >> tpgid >> flags >> minflt >> cminflt
         >> majflt >> cmajflt >> utime >> stime >> cutime >> cstime >> priority >> nice >> cur_proc_num_threads
         >> itrealvalue >> starttime >> cur_virt_size >> rss; // don't care about the rest
 
     stat_stream.close();
-
-    Int64 page_size_kb = sysconf(_SC_PAGE_SIZE) / 1024; // in case x86-64 is configured to use 2MB pages
-    resident_set = rss * page_size_kb;
     return true;
 }
 
@@ -90,16 +87,15 @@ void CollectProcInfoBackgroundTask::memCheckJob()
 {
     try
     {
-        double resident_set;
         Int64 cur_proc_num_threads = 1;
-        UInt64 cur_virt_size = 0;
         while (!end_syn)
         {
-            process_mem_usage(resident_set, cur_proc_num_threads, cur_virt_size);
-            resident_set *= 1024; // unit: byte
-            real_rss = static_cast<Int64>(resident_set);
+            auto metrics = get_process_metrics();
+            process_num_threads(cur_proc_num_threads);
+            real_rss = static_cast<Int64>(metrics.rss);
+            real_rss_file = static_cast<Int64>(metrics.rss_file);
             proc_num_threads = cur_proc_num_threads;
-            proc_virt_size = cur_virt_size;
+            proc_virt_size = metrics.vsize;
             baseline_of_query_mem_tracker = root_of_query_mem_trackers->get();
             usleep(100000); // sleep 100ms
         }
